b550ff944b5bbc74970dce6430b7da1e16ee49c8
“deuro” <hero14yki1986@gmail.com>
2021-07-18 11:02:29 +0000

    Finish add-user-attendances

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+  has_many :attendances, dependent: :destroy
   attr_accessor :remember_token
   before_save {self.email = email.downcase}
   
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   validates :department, length: {in: 2..30}, allow_blank: true
   validates :basic_time, presence: true
   validates :work_time, presence: true
+  
   
   
   
